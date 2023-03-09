936638b7e0e9c6feaa054cc0588c6e3b918a142e
MilanoGamezzYT <99067157@mydavinci.nl>
2023-03-09 12:08:04 +0100

    JavaScript

--- Module 01 - JavaScript Crash Course/Horeca/Horeca.js
+++ Module 01 - JavaScript Crash Course/Horeca/Horeca.js
@@ -1,21 +1,23 @@
 const dranken = {"bier": 3.50, "fris": 3.20 , "wijn": 4.75};
 const bonnetje = {};
 
-
-let total = 0;
+let totale_prijs = 0;
 let stop = false;
 let userInput;
 let productAmount = NaN;
 let amountNaN = true;
 
 while (!stop) {
-    userInput = prompt(`Wat zou u willen bestellen? (Type 'stop' om het te beeindigen.)\nDeze dranken zijn niet op voorraad: ${Object.keys(dranken)}`);
+    userInput = prompt(`Wat zou u willen bestellen? (Type 'stop' om de bestelling te beëndigen.) Deze dranken zijn op voorraad: ${Object.keys(dranken)}`);
+
     if (userInput === "stop") {
         stop = true;
-    } else {
-        if ((userInput in dranken)) {
-            alert(`Momenteel is (${userInput}) uitverkocht. Sorry voor dit ongemak.`);
-        } else {
+    }
+    else {
+        if (!(userInput in dranken)) {
+            alert(`Momenteel is ${userInput} uitverkocht. Sorry voor dit ongemak.`);
+        }
+        else {
             while (amountNaN) {
                 productAmount = parseInt(prompt(`Hoeveel ${userInput} wilt u bestellen?`));
                 if (!isNaN(productAmount)) {
@@ -27,31 +29,24 @@ while (!stop) {
 
             if (userInput in bonnetje) {
                 bonnetje[userInput] += productAmount;
-            } else {
+            }
+            else {
                 bonnetje[userInput] = productAmount;
             }
-            if (userInput === "cola", "sinas", "ice tea") {
-                dranken[userInput] = dranken["fris"];
-            }
-            if (userInput === "leffe dubbel", "grolsch", "hertog jan") {
-                dranken[userInput] = dranken["bier"];
-            }
-            if (userInput === "chardonnay", "mocen", "rose" , "prosecco") {
-                dranken[userInput] = dranken["wijn"];
-            }
-            total += productAmount * dranken[userInput];
+
+            totale_prijs += productAmount * dranken[userInput];
         }
     }
 }
 
 for (const [key, value] of Object.entries(bonnetje)) {
-    const display = `${value}x ${key} - €${(value*dranken[key]).toFixed(2)}`
+    const display = `${value}x ${key} - €${(value * dranken[key]).toFixed(2)}`
     document.getElementById("Bonnetje").innerText = "Uw Bonnetje - Bedankt voor het bestellen!";
     document.getElementById("Dranken").innerText += display + '\n';
 }
 
-if (total > 0) {
-    document.getElementById("Totale_prijs").innerText = "Total: €" + total.toFixed(2);
+if (totale_prijs > 0) {
+    document.getElementById("Totale_prijs").innerText = "Totaal: €" + totale_prijs.toFixed(2);
 }
 
 
