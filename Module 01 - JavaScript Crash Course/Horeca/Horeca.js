f599560ebcc29ed76ed77105884950edf21bb60a
MilanoGamezzYT <99067157@mydavinci.nl>
2023-03-16 14:46:21 +0100

    JS

--- Module 01 - JavaScript Crash Course/Horeca/Horeca.js
+++ Module 01 - JavaScript Crash Course/Horeca/Horeca.js
@@ -34,14 +34,14 @@ function bon(){
     document.getElementById("Bonnetje_tekst").innerHTML = "Producten:";  
     document.getElementById("Dranken").innerHTML = bon;
 }
-  
+
 while(meer){
     vraag = prompt("Wat wilt u bestellen? Type 'Stop' om te stoppen. U heeft keuze tussen bier, fris en een wijntje").toLowerCase()
     if (vraag == "stop"){
         meer = false;
     }else if (drankjes.includes(vraag)){
         aantal = parseInt(prompt("Hoeveel van " + vraag + " wilt u hebben?"));
-        if (isFinite(aantal)){
+        if (aantal){
             drankjeToevoegen(vraag, aantal);
         } else{
             alert("Dat is geen getal! ");
@@ -52,6 +52,6 @@ while(meer){
 }
 if (!meer){
     bon();
-    document.getElementById("Bonnetje").innerHTML = "<b>Totaal prijs: " + "€" + totaalPrijs.toFixed(2);
+    document.getElementById("Bonnetje").innerHTML = "Totaal prijs: " + "€" + totaalPrijs.toFixed(2);
     document.getElementById("Groet").innerText = "Graag tot ziens, en een fijne dag verder!";
 }
